12cb6409ba4ac251dea36b654294b4d84c48c6a2
SantiagoLiraGarcia <alpha.razie@gmail.com>
2023-05-03 10:39:43 -0700

    Minor changes

--- Assets/Scripts/Birthday.cs
+++ Assets/Scripts/Birthday.cs
@@ -7,7 +7,29 @@ public class Birthday : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i;
+        for (i = 1; i <= 12; i++) 
+        {
+            if (i == 1) print("January");
+            if (i == 2) print("February");
+            if (i == 3) print("March");
+            if (i == 4) print("April");
+            if (i == 5) print("May");
+            if (i == 6) print("June");
+            if (i == 7) print("July");
+            if (i == 8) print("August");
+            if (i == 9) print("September");
+            if (i == 10) print("It's my birthday month!");
+            if (i == 11) print("November");
+            if (i == 12) print("December");
+        }
+                 int p;
+        for (p = 1; p <= 31; p++)
+        {
+           
+             if (p == 10) {print("It's my Birthday!");}
+             else { Debug.Log(p);}
+        }
     }
 
     // Update is called once per frame
